ca63e54c6c7280b81f6702f3ac87d5171e7d3e8e
Caleb-Leung-Kwan-Ho <107651697+Caleb-Leung-Kwan-Ho@users.noreply.github.com>
2022-08-04 14:08:32 -0400

    Add files via upload

---
+++ My-personal-project/mywebsite/Interest/models.py
@@ -0,0 +1,13 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Interest(models.Model):
+    Hobby = models.CharField(max_length=100)
+    slug = models.SlugField()
+    rank = models.PositiveSmallIntegerField()    
+    date = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.Hobby
