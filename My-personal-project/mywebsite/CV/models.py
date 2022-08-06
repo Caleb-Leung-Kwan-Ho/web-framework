56aabf9ffaa633032ce0a5f4adc7bfa9785cf1d9
Caleb-Leung-Kwan-Ho <caleb.leungkwanho@gmail.com>
2022-08-05 20:22:00 -0400

    try split tree

--- My-personal-project/mywebsite/CV/models.py
+++ My-personal-project/mywebsite/CV/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class CV(models.Model):
+    title = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date= models.DateField()
+    details = models.TextField()
+
+    def __str__(self):
+        return self.title
